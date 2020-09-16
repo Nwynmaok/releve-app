467fc5849feab7f3947abbd870643b15200c3730
Nathan Kaufman <Nwynmaok@gmail.com>
2020-09-15 20:44:42 -0500

    entry point folder

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
+import App from "./src/App.js"
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { registerRootComponent } from 'expo';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class Main extends React.Component {
+  render() {
+    return (
+      <App />
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+registerRootComponent(App);
